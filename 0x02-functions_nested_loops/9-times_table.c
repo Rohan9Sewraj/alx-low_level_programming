fb87b3654b3e20d45ac94fa6b8597a059ed382f0
Rohan Sewraj <sewrajrohan33@gmail.com>
2023-02-21 17:05:28 +0200

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,40 +1,51 @@
 #include "main.h"
-
 /**
-  * times_table - Prints the 9 times table, starting with 0
-  * Return: empty output
+  * times_table - The entry point of the function
+  * @void: The function takes no parameters
+  *
+  * Description: This is a function that prints the 9 times table, starting
+  * with 0.
   */
 void times_table(void)
-
 {
+	int firstnum, secondnum, multiplication_result;
 
-        int x, y, z, u, d;
-
-        for (x = 0; x <= 9; x++)
-        {
-        for (y = 0; y <= 9; y++)
-        {
-        z = x * y;
-        if (z > 9)
-        {
-        u = z % 10;
-        d = (z - u) / 10;
-        _putchar(44);
-        _putchar(32);
-        _putchar(d + '0');
-        _putchar(u + '0');
-        }
-        else
-        {
-        if (y != 0)
-        {
-        _putchar(44);
-        _putchar(32);
-        _putchar(32);
-        }
-        _putchar(z + '0');
-        }
-        }
-        _putchar('\n');
-        }
+	firstnum = 0;
+	while (firstnum < 10)
+	{
+		secondnum = 0;
+		while (secondnum < 10)
+		{
+			multiplication_result = firstnum * secondnum;
+			if (secondnum != 9)
+			{
+				if ((multiplication_result / 10) == 0)
+				{
+					_putchar(multiplication_result + '0');
+					_putchar(',');
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar((multiplication_result / 10) + '0');
+					_putchar((multiplication_result % 10) + '0');
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				if ((multiplication_result / 10) == 0)
+					_putchar(multiplication_result + '0');
+				else
+				{
+					_putchar((multiplication_result / 10) + '0');
+					_putchar((multiplication_result % 10) + '0');
+				}
+			}
+			secondnum++;
+		}
+		firstnum++;
+		_putchar('\n')
+	}
 }
