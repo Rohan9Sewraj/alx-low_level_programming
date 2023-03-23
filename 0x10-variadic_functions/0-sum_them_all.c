78340a760dd963c15318a71d6d4099bf44a21afc
Rohan Sewraj <sewrajrohan33@gmail.com>
2023-03-23 14:53:01 +0200

    var

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,25 +1,40 @@
-#include <stdio.h>
-#include <stdarg.h>
+/*****************************************************************************/
+/*                                                                           */
+/*                                               _____  ______    ____  ___  */
+/* 0-memset.c                                   /  _  \ |    |    \   \/  /  */
+/*                                             /  /_\  \|    |     \     /   */
+/* By: Barahmou   <hamabarhamou@gmail.com>    /    |    \    |___  /     \   */
+/*                                            \____|__  /_______ \/___/\  \  */
+/* Created: 2022-03-28 09:44:03   $Barahmou           \/        \/      \_/  */
+/* Updated: 2022-03-28 09:44:03 by Barahmou                                  */
+/*                                                                           */
+/*****************************************************************************/
+
+#include<stdio.h>
+#include<stdarg.h>
+#include<stdio.h>
 
 /**
-* sum_them_all - sum of all its parameters
-* @n: n args
-* Return: sum or if n == 0, return 0
-*/
+ * sum_them_all - a function ...
+ * @n: the chaine
+ * @...: the char
+ *
+ * Return: 1 or 0
+ */
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum = 0;
-	unsigned int i;
-	va_list arguments;
+	va_list ap;
+	unsigned int i, som = 0;
 
-	if (n)
-	{
-		va_start(arguments, n);
-		for (i = 0; i < n; i++)
-		{
-			sum += va_arg(arguments, int);
-		}
-		va_end(arguments);
-	}
-	return (sum);
+	if (n == 0)
+		return (0);
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+		som = som + va_arg(ap, int);
+
+	va_end(ap);
+	return (som);
 }
+
