b37f8a0690c4dba8d7ea7e411a6589d78609fafb
Rohan Sewraj <sewrajrohan33@gmail.com>
2023-04-08 08:01:38 +0200

    var

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,38 +1,49 @@
+/*****************************************************************************/
+/*                                                                           */
+/*                                               _____  ______    ____  ___  */
+/* 0-print_list.c                               /  _  \ |    |    \   \/  /  */
+/*                                             /  /_\  \|    |     \     /   */
+/* By: Barahmou   <hamabarhamou@gmail.com>    /    |    \    |___  /     \   */
+/*                                            \____|__  /_______ \/___/\  \  */
+/* Created: 2022-03-28 09:44:03   $Barahmou           \/        \/      \_/  */
+/* Updated: 2022-03-28 09:44:03 by Barahmou                                  */
+/*                                                                           */
+/*****************************************************************************/
+
+#include <stdio.h>
 #include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+#define BUF_SIZE 9096
 
 /**
- * create_file - creates a file
- * @filename: filename.
- * @text_content: content writed in the file.
+ * create_file - a function ...
+ * @filename: the list
+ * @text_content: the number
  *
- * Return: 1 if it success. -1 if it fails.
+ * Return: 1 or 0
  */
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	int nletters;
-	int rwr;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDWR, 600);
 
 	if (fd == -1)
 		return (-1);
 
-	if (!text_content)
-		text_content = "";
+	chmod(filename, 644);
 
-	for (nletters = 0; text_content[nletters]; nletters++)
-		;
-
-	rwr = write(fd, text_content, nletters);
-
-	if (rwr == -1)
-		return (-1);
-
+	if (text_content != NULL)
+		write(fd, text_content, strlen(text_content));
 	close(fd);
-
 	return (1);
 }
+
